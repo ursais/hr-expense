b915bf39789922e8597ff5ee386312b5fbb0dcf2
Daniel Reis <dreis@opensourceintegrators.com>
2021-12-03 18:20:09 +0000

    wip hr_expense_journal

--- hr_expense_journal/__manifest__.py
+++ hr_expense_journal/__manifest__.py
@@ -2,21 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "HR Expense Journal",
+    "name": "Select Expense Journal",
     "version": "14.0.1.0.0",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
-    "summary": "Expenses that are submitted are also paid by corporate credit cards.\
-     Users would like to create expense reports and specify payment type,\
-     i.e. Credit Card, Bank, Employee etc. Presently the option of payment\
-    is available after Expense report is created under Other info tab.",
+    "summary": "Set the Journal for the payment type used to pay the expense",
     "website": "https://github.com/OCA/hr-expense",
     "license": "AGPL-3",
     "depends": ["hr_expense"],
-    "category": "HR",
+    "category": "Human Resources/Expenses",
     "data": [
         "views/hr_expense_views.xml",
     ],
     "installable": True,
-    "maintainer": "dreispt",
+    "maintainers": ["dreispt"],
     "development_status": "Beta",
 }
